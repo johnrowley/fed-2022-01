a46e8b2c69dc2e6d60356d74e3813ac48cd73a35
John Rowley <outsidethebox@eircom.net>
2022-03-01 21:12:11 +0000

    added an example of writing out a date

--- week3/js/site.js
+++ week3/js/site.js
@@ -1,8 +1,26 @@
 function init() {
     console.log('Page has loaded')
+    WriteParagraph();
+    WriteDate();
 
-let paragraph = document.getElementById("ExampleParagraph");
 
-paragraph.innerHTML="Hello World from Javascript"
+
+}
+
+function WriteParagraph() {
+    let paragraph = document.getElementById("ExampleParagraph");
+
+    paragraph.innerHTML = "Hello World from Javascript"
+
+}
+
+function WriteDate() {
+
+    let currentDate = new Date();
+    const longDate = currentDate.toString();
+    const shortDate = currentDate.toDateString();
+    let paragraph = document.getElementById("CurrentDate");
+
+    paragraph.innerHTML = `${longDate} : ${shortDate}`
 
 }
